73c63fc84e30f1fdaab6c22d62b5c2e99b9d4842
Denis Leal <denisleal@msn.com>
2019-10-25 08:19:30 -0300

    Bishop

---
+++ src/chess/pieces/Bishop.java
@@ -0,0 +1,79 @@
+package chess.pieces;
+
+import boardgame.Board;
+import boardgame.Position;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Bishop extends ChessPiece {
+
+	public Bishop(Board board, Color color) {
+		super(board, color);
+		
+	}
+	
+	@Override
+	public String toString() {
+		return "B";
+	}
+
+	@Override
+	public boolean[][] possibleMoves() {
+boolean [][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		
+		Position p = new Position(0,0);
+		
+		// check up left diagonal (NW)
+		p.setValues(position.getRow() - 1, position.getColumn()-1);
+		while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) { // enquanto a posição for válida e não tiver uma peça
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setRow(p.getRow()-1);
+			p.setColumn(p.getColumn()-1);
+		}
+		if(getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		// check up right diagonal (NE)
+		p.setValues(position.getRow()-1 , position.getColumn()+ 1);
+		while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) { // enquanto a posição for válida e não tiver uma peça
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setValues(p.getRow()-1, p.getColumn()+1);
+			/*p.setRow(p.getRow()-1);
+			p.setColumn(p.getColumn()+1); */
+			
+		}
+		if(getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		// check down right (NE)
+		p.setValues(position.getRow()+1 , position.getColumn()+ 1);
+		while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) { // enquanto a posição for válida e não tiver uma peça
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setRow(p.getRow()+1);
+			p.setColumn(p.getColumn()+1);
+		}
+		if(getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		// check down left (NW)
+		p.setValues(position.getRow() + 1, position.getColumn()-1);
+		while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) { // enquanto a posição for válida e não tiver uma peça
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setRow(p.getRow()+1);
+			p.setColumn(p.getColumn()+1);
+			
+		}
+		if(getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		
+		return mat;
+
+	}
+	
+
+}
