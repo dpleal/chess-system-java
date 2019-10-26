619b3786ce0c31808856e83aa61b609ec21d339c
Denis Leal <denisleal@msn.com>
2019-10-26 10:45:37 -0300

    Queen

--- src/chess/pieces/Queen.java
+++ src/chess/pieces/Queen.java
@@ -5,23 +5,69 @@ import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
-public class Bishop extends ChessPiece {
-
-	public Bishop(Board board, Color color) {
-		super(board, color);
-		
-	}
+public class Queen extends ChessPiece {
 	
+	
+	public Queen(Board board, Color color) {
+		super(board, color);
+	}
+
 	@Override
 	public String toString() {
-		return "B";
+		return "Q";
 	}
 
 	@Override
 	public boolean[][] possibleMoves() {
-boolean [][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		
+		
+		boolean [][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
 		
 		Position p = new Position(0,0);
+		
+		
+		// MOVIMENTOS DE TORRE
+		// check above (só na vertical)
+		p.setValues(position.getRow() - 1, position.getColumn());
+		while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) { // enquanto a posição for válida e não tiver uma peça
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setRow(p.getRow()-1);
+		}
+		if(getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		// check left (só na horizontal)
+		p.setValues(position.getRow() , position.getColumn()- 1);
+		while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) { // enquanto a posição for válida e não tiver uma peça
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setColumn(p.getColumn()-1);
+		}
+		if(getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		// check right (só na horizontal)
+		p.setValues(position.getRow() , position.getColumn()+ 1);
+		while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) { // enquanto a posição for válida e não tiver uma peça
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setColumn(p.getColumn()+1);
+		}
+		if(getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		// check below (só na vertical)
+		p.setValues(position.getRow() + 1, position.getColumn());
+		while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) { // enquanto a posição for válida e não tiver uma peça
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setRow(p.getRow()+1);
+		}
+		if(getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		// MOVIMENTOS DE BISPO
 		
 		// check up left diagonal (NW)
 		p.setValues(position.getRow() - 1, position.getColumn()-1);
@@ -74,6 +120,4 @@ boolean [][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
 		return mat;
 
 	}
-	
-
 }
